ecb901afc3df9e39cbe1befa8c3ce4437e033dc5
Martin Provencher <mprovencher86@gmail.com>
2012-03-07 11:19:45 -0500

    First commit with refinerycms and rails 3.2.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupeSage::Application.config.secret_token = 'ee2d5a5b4cd19dde8b83105e0e4adaaa0bc35eb0b8eb41ade5eb04eef45d098ae70c3fd99f3c774c179ce3609c77b504c60ae3e961c6c00fd9ff42c79209d553'
